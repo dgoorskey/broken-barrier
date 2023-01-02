deffb6f35b83ee3a0efcf49844ddc75e0792f39f
DersuG <dersu.goorskey@gmail.com>
2023-01-02 15:34:41 -0500

    make input buffer display look cleaner

--- scenes/components/input_buffer.gd
+++ scenes/components/input_buffer.gd
@@ -7,9 +7,10 @@ func _process(delta: float) -> void:
 	# Scan through all inputs and check if they should be added to the buffer.
 	for input in ["p1_left", "p1_right", "p1_up", "p1_down", "p1_jump", "p1_a", "p1_b"]:
 		if Input.is_action_just_pressed(input):
-			buffer.push_front(input + "_pressed")
+			buffer.push_front(input)
 		if Input.is_action_just_released(input):
-			buffer.push_front(input + "_released")
+#			buffer.push_front("- " + input)
+			pass
 	
 	# If buffer is too big, remove old inputs.
 	while buffer.size() > 20:
