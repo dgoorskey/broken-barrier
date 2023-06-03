13d16520a0e375a2d88662ccf97556b75d8e445c
DersuG <dersu.goorskey@gmail.com>
2023-06-03 15:41:28 -0400

    use child order instead of relative z-index

--- scenes/components/flip_sort_2d.gd
+++ scenes/components/flip_sort_2d.gd
@@ -2,8 +2,7 @@
 # z-sorts children based on the state of a Flip2D
 # - no change when facing right
 # - inverted when facing left
-# - will set `z_as_relative` to true on children
-# - will modify `z_index` of children
+# - will change the order of children
 #-------------------------------------------------------------------------------
 extends Node2D
 class_name FlipSort2D
@@ -11,29 +10,10 @@ class_name FlipSort2D
 @export var flip_2d: Flip2D
 
 func _ready() -> void:
-	flip_2d.flipped_left.connect(sort_left)
-	flip_2d.flipped_right.connect(sort_right)
+	flip_2d.flipped_left.connect(flip_sort)
+	flip_2d.flipped_right.connect(flip_sort)
 
-# z-sorts children as if the Flip2D is facing right
-func sort_right() -> void:
-	var z := 0
-	var children := get_children()
-	children.reverse() # last child should have highest z-index
-	for child in children:
-		if child is CanvasItem:
-			child = child as CanvasItem
-			child.z_as_relative = true
-			child.z_index = z
-		z -= 1
-
-# z-sorts children as if the Flip2D is facing left
-func sort_left() -> void:
-	var z := 0
-	var children := get_children()
-#	children.reverse()
-	for child in children:
-		if child is CanvasItem:
-			child = child as CanvasItem
-			child.z_as_relative = true
-			child.z_index = z
-		z -= 1
+# flips the order of all children
+func flip_sort() -> void:
+	for child in get_children():
+		move_child(child, -1)
