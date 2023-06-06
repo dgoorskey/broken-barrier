fd44af4f9ac196f06fec853c7fc27415073a2578
DersuG <dersu.goorskey@gmail.com>
2023-06-06 00:17:26 -0400

    move SimpleAttack-specific code out of Move

--- scenes/components/simple_attack.gd
+++ scenes/components/simple_attack.gd
@@ -3,12 +3,16 @@
 #-------------------------------------------------------------------------------
 extends Move
 
+@export var animator: AnimationTree
+
 @export var trigger_animation: StringName # the animation that will trigger this move
 @export var hitbox: Hitbox = null # the hitbox to activate when this move starts. if null, uses the first child.
 
 @export var damage := 0.0
 
 func _ready() -> void:
+	animator.animation_started.connect(on_animation_started)
+	
 	if hitbox == null and get_child(0) is Hitbox:
 		hitbox = get_child(0)
 
